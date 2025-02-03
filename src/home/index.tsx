041652ea28c1b59dd8454ce53cecf9795a1d6f5b
Vinicius Karabolsak <viniciuskarabolsak@hotmail.com>
2025-02-03 18:37:05 -0300

    Transições home

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,10 +1,58 @@
+import './style.css'
+
+import Usuario from './usuario/usuario.jpg';
+import Logo from './usuario/logo.png';
+import { motion } from 'framer-motion';
+
 
 export default function Home () {
     return (
-
         <>
-        <div>
-            <h1>Home</h1>
+        <div className='principal'>
+            <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+                >
+                <h3>Olá, Gabriel</h3>
+            </motion.div>    
+            <div className='perfil-container'>
+                <motion.div 
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5, delay: 0.3 }}>
+                    <img 
+                        src={Usuario} 
+                        alt="usuario-foto" 
+                        className='usuario-foto' 
+                    />
+                </motion.div>
+                <motion.div 
+                    className='info-container'
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}>
+                    <div className="ranking-container">
+                        <p>Ranking</p> 
+                        <motion.img 
+                            src={Logo} 
+                            alt="logo-ranking" 
+                            className="ranking-logo"
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 0.5, delay: 0.6 }}
+                        />
+                    </div>                    
+                </motion.div>
+                <motion.div 
+                        className="jogos-container"
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: 0.7 }}>
+                        <p>Jogos</p>
+                        <p>100.000</p>
+                </motion.div>
+            </div>
         </div>
         </>
     )
