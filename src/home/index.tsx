87cc6cc8efc06731aa6d8479e42ea1630b75412a
Vinicius Karabolsak <viniciuskarabolsak@hotmail.com>
2025-02-08 11:39:34 -0300

    navegacao

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,12 +1,34 @@
 import './style.css'
-import Usuario from './usuario/usuario.jpg';
+import Usuario from '../home/usuario/usuario.jpg';
 import Logo from './usuario/logo.png';
 import { motion } from 'framer-motion';
 import Menu from './constante.tsx'
 import Tab from '../superior/superior.tsx';
-import Nav from '../navegacao/index.tsx'
+
+import jogos from '../../public/assets/joystick_1.png';
+import estatistica from '../../public/assets/statistics_1.png';
+import conectado from '../../public/assets/conection_1.png';
+import mission from '../../public/assets/mission_1.png';
+import home from '../../public/assets/home_1.png';
+
+import jogosAtivo from '../../public/assets/joystick_2.png';
+import estatisticaAtivo from '../../public/assets/statistics_2.png';
+import conectadoAtivo from '../../public/assets/conection_2.png';
+import homeAtivo from '../../public/assets/home_2.png';
+import missionAtivo from '../../public/assets/mission_2.png';
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Home () {
+    const [active, setActive] = useState<string>("home");
+    const navigate = useNavigate();
+
+    const handleNavigation = (path: string, name: string) => {
+        setActive(name);
+        navigate(path);
+    };
+
     return (
         <>
         <div className='containerTop'>
@@ -144,9 +166,50 @@ export default function Home () {
                     <Menu />
                 </motion.div>
         </div>
-        <div className='containerBot'>
-            <Nav />
-        </div>
+        <nav className="bottom-nav">
+      <ul>
+        <li>
+          <img
+            src={active === "jogos" ? jogosAtivo : jogos}
+            alt="jogos"
+            className={`navegadores ${active === "jogos" ? "active" : ""}`}
+            onClick={() => handleNavigation("/jogos", "jogos")}
+          />
+        </li>
+        <li>
+          <img
+            src={active === "estatistica" ? estatisticaAtivo : estatistica}
+            alt="estatisticas"
+            className={`navegadores ${active === "estatistica" ? "active" : ""}`}
+            onClick={() => handleNavigation("/estatistica", "estatistica")}
+          />
+        </li>
+        <li>
+          <img
+            src={active === "conectado" ? conectadoAtivo : conectado}
+            alt="conexões"
+            className={`navegadores ${active === "conectado" ? "active" : ""}`}
+            onClick={() => handleNavigation("/conectado", "conectado")}
+          />
+        </li>
+        <li>
+          <img
+            src={active === "mission" ? missionAtivo : mission}
+            alt="configuracoes"
+            className={`navegadores ${active === "mission" ? "active" : ""}`}
+            onClick={() => handleNavigation("/mission", "mission")}
+          />
+        </li>
+        <li>
+          <img
+            src={active === "home" ? homeAtivo : home}
+            alt="home"
+            className={`navegadores ${active === "home" ? "active" : ""}`}
+            onClick={() => handleNavigation("/home", "home")}
+          />
+        </li>
+      </ul>
+    </nav>
     </>
     )
 }
